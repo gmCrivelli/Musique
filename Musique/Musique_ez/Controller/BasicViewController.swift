78b8832c532637ca8e4df96907e989141dd7888d
Rafael Tomaz Prado <rafael.tomaz.prado@gmail.com>
2017-11-28 14:28:08 -0200

    Created BaseViewController to be extended by others

---
+++ Musique/Musique_ez/Controller/BasicViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BasicViewController.swift
+//  Musique_ez
+//
+//  Created by Rafael Prado on 28/11/17.
+//  Copyright © 2017 Gustavo De Mello Crivelli. All rights reserved.
+//
+
+import UIKit
+
+class BasicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
