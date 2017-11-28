458e15c0f1a11e37d8b9827766a8d7b9485ea263
Rafael Tomaz Prado <rafael.tomaz.prado@gmail.com>
2017-11-28 16:05:05 -0200

    Added and implemented MusicSelection VC
    Basic features, like returning to previous page are working.

---
+++ Musique/Musique_ez/Controller/MusicSelectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MusicSelectionViewController.swift
+//  Musique_ez
+//
+//  Created by Rafael Prado on 28/11/17.
+//  Copyright © 2017 DoM7. All rights reserved.
+//
+
+import UIKit
+
+/// Manages the Music Selection screen before the Pulse activity
+class MusicSelectionViewController: BasicViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func goBack(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+}
