3a4ebb2c46dd500c79b57e35ebc1c46782ca52f2
Unknown <ederaldo.ratz@fatec.sp.gov.br>
2017-11-29 21:07:49 -0200

    Added DAO mechanism.

---
+++ Musique/Musique_ez/Persistence/DAO/PulseGameDAO.swift
@@ -0,0 +1,87 @@
+//
+//  PulseGameDAO.swift
+//  Musique_ez
+//
+//  Created by Ederaldo Ratz on 29/11/2017.
+//  Copyright © 2017 Gustavo De Mello Crivelli. All rights reserved.
+//
+
+import Foundation
+
+
+import Foundation
+import CoreData
+
+class SeasonDAO {
+    
+    /// Method responsible for saving a season into database
+    /// - parameters:
+    ///     - objectToBeSaved: season to be saved on database
+    /// - throws: if an error occurs during saving an object into database (Errors.DatabaseFailure)
+    static func create(_ objectToBeSaved: Pulse) throws {
+        do {
+            // add object to be saved to the context
+            CoreDataManager.sharedInstance.persistentContainer.viewContext.insert(objectToBeSaved)
+            
+            // persist changes at the context
+            try CoreDataManager.sharedInstance.persistentContainer.viewContext.save()
+        }
+        catch {
+            throw Errors.DatabaseFailure
+        }
+    }
+    
+    /// Method responsible for updating a season into database
+    /// - parameters:
+    ///     - objectToBeUpdated: season to be updated on database
+    /// - throws: if an error occurs during updating an object into database (Errors.DatabaseFailure)
+    static func update(_ objectToBeUpdated: Pulse) throws {
+        do {
+            // persist changes at the context
+            try CoreDataManager.sharedInstance.persistentContainer.viewContext.save()
+        }
+        catch {
+            throw Errors.DatabaseFailure
+        }
+    }
+    
+    /// Method responsible for deleting a season from database
+    /// - parameters:
+    ///     - objectToBeSaved: season to be saved on database
+    /// - throws: if an error occurs during deleting an object into database (Errors.DatabaseFailure)
+    static func delete(_ objectToBeDeleted: Pulse) throws {
+        do {
+            // delete element from context
+            CoreDataManager.sharedInstance.persistentContainer.viewContext.delete(objectToBeDeleted)
+            
+            // persist the operation
+            try CoreDataManager.sharedInstance.persistentContainer.viewContext.save()
+        }
+        catch {
+            throw Errors.DatabaseFailure
+        }
+    }
+    
+    /// Method responsible for gettings all seasons from database
+    /// - returns: a list of seasons from database
+    /// - throws: if an error occurs during getting an object from database (Errors.DatabaseFailure)
+    static func findAll() throws -> [Pulse] {
+        // list of seasons to be returned
+        var musicList:[Pulse]
+        
+        do {
+            // creating fetch request
+            let request:NSFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Pulse")
+            
+            // perform search
+            musicList = try CoreDataManager.sharedInstance.persistentContainer.viewContext.fetch(request) as! [Pulse]
+        }
+        catch {
+            throw Errors.DatabaseFailure
+        }
+        
+        return musicList
+    }
+    
+}
+
