88151b2424ae0f8b8ed29c2f5923c97a0924fea1
Ederaldo <ederaldo.ratz@fatec.sp.gov.br>
2017-12-01 10:09:23 -0200

    Implement ProgressServices methods for managing the Progress and update MusicServices

--- Musique/Musique_ez/Service/ProgressServices.swift
+++ Musique/Musique_ez/Service/ProgressServices.swift
@@ -9,5 +9,132 @@
 import Foundation
 
 class ProgressServices {
+    /// Function responsible for creating a progress object
+    /// - parameters:
+    ///     - progress: progress to be saved
+    ///     - completion: closure to be executed at the end of this method
+    /// - throws: if an error occurs during saving an object into database (Errors.DatabaseFailure)
+    static func createProgress(progress: ProgressPulse, _ completion: ((_ error: Error?) -> Void)?) {
+        // block to be executed in background
+        let blockForExecutionInBackground: BlockOperation = BlockOperation(block: {
+            // error to be returned in case of failure
+            var raisedError: Error? = nil
+            
+            do {
+                // save information
+                try ProgressDAO.create(progress)
+            }
+            catch let error {
+                raisedError = error
+            }
+            
+            // completion block execution
+            if (completion != nil) {
+                let blockForExecutionInMain: BlockOperation = BlockOperation(block: {completion!(raisedError)})
+                
+                // execute block in main
+                QueueManager.sharedInstance.executeBlock(blockForExecutionInMain, queueType: QueueManager.QueueType.main)
+            }
+        })
+        
+        // execute block in background
+        QueueManager.sharedInstance.executeBlock(blockForExecutionInBackground, queueType: QueueManager.QueueType.serial)
+    }
+    
+    /// Function responsible for updating a  progress object
+    /// - parameters:
+    ///     - progress: progress to be updated
+    ///     - completion: closure to be executed at the end of this method
+    /// - throws: if an error occurs during saving an object into database (Errors.DatabaseFailure)
+    static func updateProgress(progress: ProgressPulse, _ completion: ((_ error: Error?) -> Void)?) {
+        // block to be executed in background
+        let blockForExecutionInBackground: BlockOperation = BlockOperation(block: {
+            // error to be returned in case of failure
+            var raisedError: Error? = nil
+            
+            do {
+                // save information
+                try ProgressDAO.update(progress)
+            }
+            catch let error {
+                raisedError = error
+            }
+            
+            // completion block execution
+            if (completion != nil) {
+                let blockForExecutionInMain: BlockOperation = BlockOperation(block: {completion!(raisedError)})
+                
+                // execute block in main
+                QueueManager.sharedInstance.executeBlock(blockForExecutionInMain, queueType: QueueManager.QueueType.main)
+            }
+        })
+        
+        // execute block in background
+        QueueManager.sharedInstance.executeBlock(blockForExecutionInBackground, queueType: QueueManager.QueueType.serial)
+    }
+    
+    /// Function responsible for deleting a progress object
+    /// - parameters:
+    ///     - progress:  to progress to be deleted
+    ///     - completion: closure to be executed at the end of this method
+    /// - throws: if an error occurs during saving an object into database (Errors.DatabaseFailure)
+    static func deleteProgress(progress: ProgressPulse, _ completion: ((_ error: Error?) -> Void)?) {
+        // block to be executed in background
+        let blockForExecutionInBackground: BlockOperation = BlockOperation(block: {
+            // error to be returned in case of failure
+            var raisedError: Error? = nil
+            
+            do {
+                // save information
+                try ProgressDAO.delete(progress)
+            }
+            catch let error {
+                raisedError = error
+            }
+            
+            // completion block execution
+            if (completion != nil) {
+                let blockForExecutionInMain: BlockOperation = BlockOperation(block: {completion!(raisedError)})
+                
+                // execute block in main
+                QueueManager.sharedInstance.executeBlock(blockForExecutionInMain, queueType: QueueManager.QueueType.main)
+            }
+        })
+        
+        // execute block in background
+        QueueManager.sharedInstance.executeBlock(blockForExecutionInBackground, queueType: QueueManager.QueueType.serial)
+    }
+    
+    /// Function responsible for getting all progress objects
+    /// - parameters:
+    ///     - completion: closure to be executed at the end of this method
+    /// - throws: if an error occurs during getting an object from database (Errors.DatabaseFailure)
+    static func getAllProgress(_ completion: ((_ error: Error?, _ seasons: [ProgressPulse]?) -> Void)?) {
+        // block to be executed in background
+        let blockForExecutionInBackground: BlockOperation = BlockOperation(block: {
+            // error to be returned in case of failure
+            var raisedError: Error? = nil
+            var seasons: [ProgressPulse]?
+            
+            do {
+                // save information
+                seasons = try ProgressDAO.findAll()
+            }
+            catch let error {
+                raisedError = error
+            }
+            
+            // completion block execution
+            if (completion != nil) {
+                let blockForExecutionInMain: BlockOperation = BlockOperation(block: {completion!(raisedError, seasons)})
+                
+                // execute block in main
+                QueueManager.sharedInstance.executeBlock(blockForExecutionInMain, queueType: QueueManager.QueueType.main)
+            }
+        })
+        
+        // execute block in background
+        QueueManager.sharedInstance.executeBlock(blockForExecutionInBackground, queueType: QueueManager.QueueType.serial)
+    }
     
 }
