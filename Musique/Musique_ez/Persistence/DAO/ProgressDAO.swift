f3b06d03b9b2d371c9879da51e6a4487ca25d714
Ederaldo <ederaldo.ratz@fatec.sp.gov.br>
2017-12-01 09:10:31 -0200

    Created Progress DAO

--- Musique/Musique_ez/Persistence/DAO/ProgressDAO.swift
+++ Musique/Musique_ez/Persistence/DAO/ProgressDAO.swift
@@ -1,21 +1,21 @@
 //
-//  PulseGameDAO.swift
+//  ProgressDAO.swift
 //  Musique_ez
 //
-//  Created by Ederaldo Ratz on 29/11/2017.
+//  Created by Ederaldo Ratz on 01/12/2017.
 //  Copyright © 2017 Gustavo De Mello Crivelli. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-class MusicDAO {
+class ProgressDAO {
     
     /// Method responsible for saving a Music Score into database
     /// - parameters:
     ///     - objectToBeSaved: Pulse to be saved on database
     /// - throws: if an error occurs during saving an object into database (Errors.DatabaseFailure)
-    static func create(_ objectToBeSaved: MusicPulse) throws {
+    static func create(_ objectToBeSaved: ProgressPulse) throws {
         do {
             // add object to be saved to the context
             CoreDataManager.sharedInstance.persistentContainer.viewContext.insert(objectToBeSaved)
@@ -32,7 +32,7 @@ class MusicDAO {
     /// - parameters:
     ///     - objectToBeUpdated: season to be updated on database
     /// - throws: if an error occurs during updating an object into database (Errors.DatabaseFailure)
-    static func update(_ objectToBeUpdated: MusicPulse) throws {
+    static func update(_ objectToBeUpdated: ProgressPulse) throws {
         do {
             // persist changes at the context
             try CoreDataManager.sharedInstance.persistentContainer.viewContext.save()
@@ -46,7 +46,7 @@ class MusicDAO {
     /// - parameters:
     ///     - objectToBeSaved: season to be saved on database
     /// - throws: if an error occurs during deleting an object into database (Errors.DatabaseFailure)
-    static func delete(_ objectToBeDeleted: MusicPulse) throws {
+    static func delete(_ objectToBeDeleted: ProgressPulse) throws {
         do {
             // delete element from context
             CoreDataManager.sharedInstance.persistentContainer.viewContext.delete(objectToBeDeleted)
@@ -62,23 +62,24 @@ class MusicDAO {
     /// Method responsible for gettings all Musics Score from database
     /// - returns: a list of musics from database
     /// - throws: if an error occurs during getting an object from database (Errors.DatabaseFailure)
-    static func findAll() throws -> [MusicPulse] {
+    static func findAll() throws -> [ProgressPulse] {
         // list of seasons to be returned
-        var musicList:[MusicPulse]
+        var progressList:[ProgressPulse]
         
         do {
             // creating fetch request
-            let request:NSFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Music")
+            let request:NSFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Progress")
             
             // perform search
-            musicList = try CoreDataManager.sharedInstance.persistentContainer.viewContext.fetch(request) as! [MusicPulse]
+            progressList = try CoreDataManager.sharedInstance.persistentContainer.viewContext.fetch(request) as! [ProgressPulse]
         }
         catch {
             throw Errors.DatabaseFailure
         }
         
-        return musicList
+        return progressList
     }
     
 }
+
 
