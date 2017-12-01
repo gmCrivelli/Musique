2987014dacff725d1a9ac47accd065f6ca0efce2
Rafael Tomaz Prado <rafael.tomaz.prado@gmail.com>
2017-12-01 09:18:25 -0200

    Created the Services Classes for managing the objects

---
+++ Musique/Musique_ez/Service/MusicServices.swift
@@ -0,0 +1,13 @@
+//
+//  MusicService.swift
+//  Musique_ez
+//
+//  Created by Rafael Prado on 01/12/17.
+//  Copyright © 2017 Gustavo De Mello Crivelli. All rights reserved.
+//
+
+import Foundation
+
+class MusicServices {
+    
+}
---
+++ Musique/Musique_ez/Service/ProgressServices.swift
@@ -0,0 +1,13 @@
+//
+//  Progress.swift
+//  Musique_ez
+//
+//  Created by Rafael Prado on 01/12/17.
+//  Copyright © 2017 Gustavo De Mello Crivelli. All rights reserved.
+//
+
+import Foundation
+
+class ProgressServices {
+    
+}
