f236a4a14e62a7b4c450f9ef3de2ba4aef91fe65
Rafael Tomaz Prado <rafael.tomaz.prado@gmail.com>
2017-12-01 09:33:51 -0200

    Implemented MusicServices methods for managing the objects

--- Musique/Musique_ez/Service/MusicServices.swift
+++ Musique/Musique_ez/Service/MusicServices.swift
@@ -10,4 +10,131 @@ import Foundation
 
 class MusicServices {
     
+    /// Function responsible for creating a music object
+    /// - parameters:
+    ///     - music: music to be saved
+    ///     - completion: closure to be executed at the end of this method
+    /// - throws: if an error occurs during saving an object into database (Errors.DatabaseFailure)
+    static func createMusic(music: MusicPulse, _ completion: ((_ error: Error?) -> Void)?) {
+        // block to be executed in background
+        let blockForExecutionInBackground: BlockOperation = BlockOperation(block: {
+            // error to be returned in case of failure
+            var raisedError: Error? = nil
+            
+            do {
+                // save information
+                try MusicDAO.create(music)
+            }
+            catch let error {
+                raisedError = error
+            }
+            
+            // completion block execution
+            if (completion != nil) {
+                let blockForExecutionInMain: BlockOperation = BlockOperation(block: {completion!(raisedError)})
+                
+                // execute block in main
+                QueueManager.sharedInstance.executeBlock(blockForExecutionInMain, queueType: QueueManager.QueueType.main)
+            }
+        })
+        
+        // execute block in background
+        QueueManager.sharedInstance.executeBlock(blockForExecutionInBackground, queueType: QueueManager.QueueType.serial)
+    }
+    
+    /// Function responsible for updating a music object
+    /// - parameters:
+    ///     - music: music to be updated
+    ///     - completion: closure to be executed at the end of this method
+    /// - throws: if an error occurs during saving an object into database (Errors.DatabaseFailure)
+    static func updateMusic(music: MusicPulse, _ completion: ((_ error: Error?) -> Void)?) {
+        // block to be executed in background
+        let blockForExecutionInBackground: BlockOperation = BlockOperation(block: {
+            // error to be returned in case of failure
+            var raisedError: Error? = nil
+            
+            do {
+                // save information
+                try MusicDAO.update(music)
+            }
+            catch let error {
+                raisedError = error
+            }
+            
+            // completion block execution
+            if (completion != nil) {
+                let blockForExecutionInMain: BlockOperation = BlockOperation(block: {completion!(raisedError)})
+                
+                // execute block in main
+                QueueManager.sharedInstance.executeBlock(blockForExecutionInMain, queueType: QueueManager.QueueType.main)
+            }
+        })
+        
+        // execute block in background
+        QueueManager.sharedInstance.executeBlock(blockForExecutionInBackground, queueType: QueueManager.QueueType.serial)
+    }
+    
+    /// Function responsible for deleting a music object
+    /// - parameters:
+    ///     - music: music to be deleted
+    ///     - completion: closure to be executed at the end of this method
+    /// - throws: if an error occurs during saving an object into database (Errors.DatabaseFailure)
+    static func deleteMusic(music: MusicPulse, _ completion: ((_ error: Error?) -> Void)?) {
+        // block to be executed in background
+        let blockForExecutionInBackground: BlockOperation = BlockOperation(block: {
+            // error to be returned in case of failure
+            var raisedError: Error? = nil
+            
+            do {
+                // save information
+                try MusicDAO.delete(music)
+            }
+            catch let error {
+                raisedError = error
+            }
+            
+            // completion block execution
+            if (completion != nil) {
+                let blockForExecutionInMain: BlockOperation = BlockOperation(block: {completion!(raisedError)})
+                
+                // execute block in main
+                QueueManager.sharedInstance.executeBlock(blockForExecutionInMain, queueType: QueueManager.QueueType.main)
+            }
+        })
+        
+        // execute block in background
+        QueueManager.sharedInstance.executeBlock(blockForExecutionInBackground, queueType: QueueManager.QueueType.serial)
+    }
+    
+    /// Function responsible for getting all music objects
+    /// - parameters:
+    ///     - completion: closure to be executed at the end of this method
+    /// - throws: if an error occurs during getting an object from database (Errors.DatabaseFailure)
+    static func getAllMusic(_ completion: ((_ error: Error?, _ seasons: [MusicPulse]?) -> Void)?) {
+        // block to be executed in background
+        let blockForExecutionInBackground: BlockOperation = BlockOperation(block: {
+            // error to be returned in case of failure
+            var raisedError: Error? = nil
+            var seasons: [MusicPulse]?
+            
+            do {
+                // save information
+                seasons = try MusicDAO.findAll()
+            }
+            catch let error {
+                raisedError = error
+            }
+            
+            // completion block execution
+            if (completion != nil) {
+                let blockForExecutionInMain: BlockOperation = BlockOperation(block: {completion!(raisedError, seasons)})
+                
+                // execute block in main
+                QueueManager.sharedInstance.executeBlock(blockForExecutionInMain, queueType: QueueManager.QueueType.main)
+            }
+        })
+        
+        // execute block in background
+        QueueManager.sharedInstance.executeBlock(blockForExecutionInBackground, queueType: QueueManager.QueueType.serial)
+    }
 }
