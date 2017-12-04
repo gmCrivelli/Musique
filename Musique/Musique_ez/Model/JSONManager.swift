544fe0218d15f4d34039d2a827d3e3bd4a1689d5
Rafael Tomaz Prado <rafael.tomaz.prado@gmail.com>
2017-12-04 13:08:06 -0200

    Added the JSON manager class

---
+++ Musique/Musique_ez/Model/JSONManager.swift
@@ -0,0 +1,13 @@
+//
+//  JSONManager.swift
+//  Musique_ez
+//
+//  Created by Rafael Prado on 04/12/17.
+//  Copyright © 2017 DoM7. All rights reserved.
+//
+
+import Foundation
+
+class JSONManager{
+    
+}
