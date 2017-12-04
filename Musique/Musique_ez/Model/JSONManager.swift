becbe908b1c5723b8f7745d8e9fb9610399f5ecc
Rafael Tomaz Prado <rafael.tomaz.prado@gmail.com>
2017-12-04 14:41:32 -0200

    Implemented json parsing

--- Musique/Musique_ez/Model/JSONManager.swift
+++ Musique/Musique_ez/Model/JSONManager.swift
@@ -9,5 +9,35 @@
 import Foundation
 
 class JSONManager{
-    
+    static func loadJson() -> [MusicPulse]{
+         var response = [MusicPulse]()
+        
+        do{
+            if let file = Bundle.main.url(forResource: "pulse", withExtension: "json"){
+                let data = try Data(contentsOf: file)
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                
+                if let songs = json as? [[String:Any]]{
+                    for song in songs{
+                        let music = MusicPulse()
+                        
+                        music.name = song["name"] as! String
+                        music.fileName = song["fileName"] as! String
+                        music.fileExtension = song["fileExtension"] as! String
+                        music.id = song["id"] as! Int16
+                        music.bpm = song["bpm"] as! Double
+                        music.highScore = song["highScore"] as! Int16
+                        music.lastScore = song["lastScore"] as! Int16
+                        
+                        response.append(music)
+                    }
+                }
+            }else{
+                print("Invalid Filepath")
+            }
+        }catch{
+            print(error.localizedDescription)
+        }
+        return response
+    }
 }
