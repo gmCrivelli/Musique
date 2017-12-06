27bfc36c36d8998f3353e4ed55291a9559380a10
Rafael Tomaz Prado <rafael.tomaz.prado@gmail.com>
2017-12-06 11:02:04 -0200

    Added a class to manage the AboutViewController

---
+++ Musique/Musique_ez/Controller/AboutViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutViewController.swift
+//  Musique_ez
+//
+//  Created by Rafael Prado on 06/12/17.
+//  Copyright © 2017 DoM7. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
